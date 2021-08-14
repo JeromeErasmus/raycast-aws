95e092ee319e3cfdf9a5b72ee59e9f283b03557a
JeromeErasmus <781052+JeromeErasmus@users.noreply.github.com>
2021-08-14 15:57:27 +1000

    adding search filtering to results

--- src/s3.py
+++ src/s3.py
@@ -15,20 +15,42 @@
 # @raycast.author Jerome Erasmus
 # @raycast.authorURL https://github.com/JeromeErasmus
 
+import sys
 import botocore
 import botostubs, boto3
 from core.aws_config import AWSConfig
+from fuzzysearch import find_near_matches
 
 config = AWSConfig()
 client = config.session.client('s3') # type: botostubs.S3
 
-def list_buckets():
+def list_buckets(search_filter):
     try:
         response = client.list_buckets()
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            for item in response['Buckets']:
+            for item in search_list(search_filter, 'Name', response['Buckets']):
                 print(item['Name'])
     except botocore.exceptions.ClientError as error:
         raise error
 
-list_buckets()
+
+def search_list(term, key, search_list):
+    ''' Searches a list for a given key and value.
+    '''
+    if(not search_list or not key or not term):
+        return search_list
+    
+    filtered = list()
+    for item in search_list:
+        if find_near_matches(term, item[key], max_l_dist=1):
+            filtered.append(item)
+    
+    return filtered
+
+
+if len(sys.argv) > 1:
+    list_buckets(sys.argv[1])
+else:
+    list_buckets(None)
+
+exit(0)
